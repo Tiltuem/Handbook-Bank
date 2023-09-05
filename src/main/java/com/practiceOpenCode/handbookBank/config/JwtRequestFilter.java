1b73a3d0d0346da39b1bfa819b4575f84650d772
Tiltuem <98845422+Tiltuem@users.noreply.github.com>
2023-09-05 20:27:22 +0400

    fix

--- src/main/java/com/practiceOpenCode/handbookBank/config/JwtRequestFilter.java
+++ src/main/java/com/practiceOpenCode/handbookBank/config/JwtRequestFilter.java
@@ -37,6 +37,8 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                     jwt = bearer.substring(7);
                     try {
                         username = jwtTokenUtils.getUsername(jwt);
+                    } catch (ExpiredJwtException e) {
+                        log.info("Время жизни токена вышло");
                     } catch (SignatureException e) {
                         log.info("Подпись неправильная");
                     }
